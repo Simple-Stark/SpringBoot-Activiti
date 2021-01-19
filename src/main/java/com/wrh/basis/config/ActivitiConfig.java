46d47187e7ab3cad15ae8b296c02e674b1f72416
王若恒 <wangruoheng@bonc.com.cn>
2021-01-19 18:23:03 +0800

    1.pom文件修改（增加activiti 画流程图的依赖） 2.整合activit Web端在线流程绘制

---
+++ src/main/java/com/wrh/basis/config/ActivitiConfig.java
@@ -0,0 +1,11 @@
+package com.wrh.basis.config;
+
+/**
+ * Activiti配置
+ * @author Simple
+ * @date 2021/1/19 17:57
+ * @change: 2021/1/19 17:57 by wangruoheng@bonc.com.cn for init
+ */
+public class ActivitiConfig {
+
+}
