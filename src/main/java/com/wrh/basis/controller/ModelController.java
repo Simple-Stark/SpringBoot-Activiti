8e8c42361e80ae1b79235f15783bc222b4ac4ca5
王若恒 <wangruoheng@bonc.com.cn>
2021-01-20 16:55:37 +0800

    1.将对模型的操作整合到一个Controller控制层

--- src/main/java/com/wrh/basis/controller/ModelController.java
+++ src/main/java/com/wrh/basis/controller/ModelController.java
@@ -3,14 +3,27 @@ package com.wrh.basis.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.activiti.editor.constants.ModelDataJsonConstants;
+import org.activiti.engine.ActivitiException;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.repository.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.activiti.rest.editor.model.ModelEditorJsonRestResource;
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.image.PNGTranscoder;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Model模型控制层
@@ -19,7 +32,10 @@ import java.io.IOException;
  * @change: 2021/1/20 9:12 by wangruoheng@bonc.com.cn for init
  */
 @RestController
-public class ModelController {
+@RequestMapping("/service")
+public class ModelController implements ModelDataJsonConstants {
+
+    protected static final Logger log = LoggerFactory.getLogger(ModelEditorJsonRestResource.class);
 
     @Resource
     private RepositoryService repositoryService;
@@ -27,9 +43,8 @@ public class ModelController {
     @Resource
     private ObjectMapper objectMapper;
 
-    @GetMapping("/newmodel")
-    public void newModel(HttpServletResponse response) throws IOException {
-        //securityUtil.logInAs("admin");
+    @GetMapping("/createModel")
+    public void createModel(HttpServletResponse response) throws IOException {
         //初始化一个空模型
         Model model = repositoryService.newModel();
 
@@ -58,8 +73,85 @@ public class ModelController {
         ObjectNode stencilSetNode = objectMapper.createObjectNode();
         stencilSetNode.put("namespace",
                 "http://b3mn.org/stencilset/bpmn2.0#");
-        editorNode.put("stencilset", stencilSetNode);
-        repositoryService.addModelEditorSource(id,editorNode.toString().getBytes("utf-8"));
+        editorNode.set("stencilset", stencilSetNode);
+        repositoryService.addModelEditorSource(id,editorNode.toString().getBytes(StandardCharsets.UTF_8));
         response.sendRedirect("/modeler.html?modelId="+id);
     }
+
+    @GetMapping("/model/{modelId}/json")
+    public ObjectNode getEditorJson(@PathVariable String modelId) {
+        ObjectNode modelNode = null;
+
+        Model model = repositoryService.getModel(modelId);
+
+        if (model != null) {
+            try {
+                if (StringUtils.isNotEmpty(model.getMetaInfo())) {
+                    modelNode = (ObjectNode) objectMapper.readTree(model.getMetaInfo());
+                } else {
+                    modelNode = objectMapper.createObjectNode();
+                    modelNode.put(MODEL_NAME, model.getName());
+                }
+                modelNode.put(MODEL_ID, model.getId());
+                ObjectNode editorJsonNode = (ObjectNode) objectMapper.readTree(
+                        new String(repositoryService.getModelEditorSource(model.getId()), StandardCharsets.UTF_8));
+                modelNode.set("model", editorJsonNode);
+
+            } catch (Exception e) {
+                log.error("Error creating model JSON", e);
+                throw new ActivitiException("Error creating model JSON", e);
+            }
+        }
+        return modelNode;
+    }
+
+    @PutMapping("/model/{modelId}/save")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void saveModel(@PathVariable String modelId, String name, String description, String json_xml,
+                            String svg_xml) {
+        try {
+
+            Model model = repositoryService.getModel(modelId);
+
+            ObjectNode modelJson = (ObjectNode) objectMapper.readTree(model.getMetaInfo());
+
+            modelJson.put(MODEL_NAME, name);
+            modelJson.put(MODEL_DESCRIPTION, description);
+            model.setMetaInfo(modelJson.toString());
+            model.setName(name);
+
+            repositoryService.saveModel(model);
+
+            repositoryService.addModelEditorSource(model.getId(), json_xml.getBytes(StandardCharsets.UTF_8));
+
+            InputStream svgStream = new ByteArrayInputStream(svg_xml.getBytes(StandardCharsets.UTF_8));
+            TranscoderInput input = new TranscoderInput(svgStream);
+
+            PNGTranscoder transcoder = new PNGTranscoder();
+            // Setup output
+            ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+            TranscoderOutput output = new TranscoderOutput(outStream);
+
+            // Do the transformation
+            transcoder.transcode(input, output);
+            final byte[] result = outStream.toByteArray();
+            repositoryService.addModelEditorSourceExtra(model.getId(), result);
+            outStream.close();
+
+        } catch (Exception e) {
+            log.error("保存模型失败！", e);
+            throw new ActivitiException("保存模型失败！", e);
+        }
+    }
+
+    @RequestMapping(value="/editor/stencilset", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
+    public @ResponseBody String getStencilset() {
+        InputStream stencilsStream = this.getClass().getClassLoader().getResourceAsStream("stencilset.json");
+        try {
+            return IOUtils.toString(stencilsStream, "utf-8");
+        } catch (Exception e) {
+            throw new ActivitiException("Error while loading stencil set", e);
+        }
+    }
+
 }
